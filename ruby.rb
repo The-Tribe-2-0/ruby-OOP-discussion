ab496c07ed07ab8a8df08ba8da81d60968ba2564
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-05-17 11:43:53 +0300

    done

---
+++ ruby.rb
@@ -0,0 +1,72 @@
+require 'pry'
+# #2
+# class Animal
+
+#     def species name
+#       "#{name}"
+#     end
+  
+# end 
+# #1
+# class Animal
+
+#     def species 
+#       "cat"
+#     end
+  
+# end 
+# dog=Animal.new
+# dog.species("dog")
+
+#3
+# class Animal
+
+#     def species
+#       "cat"
+#     end
+#   end
+  
+#   maru = Animal.new
+#   hanna = Animal.new 
+ 
+#4
+# class Animal
+#     def species 
+#       @my_species = "cat"
+#     end
+  
+#     def say_species
+#       species
+#       puts "Hi! I'm a #{@my_species}"
+#     end
+#   end
+  
+#   maru = Animal.new
+#   maru.say_species
+
+#5
+# class Animal
+#     def initialize(species)
+#       @species = species
+#     end
+  
+#     def species
+#       @species
+#     end
+#   end
+#   frederick = Animal.new("bull")
+#   frederick.species
+ 
+#6
+# class Animal
+
+#     def initialize(species)
+#       @species = species
+#     end
+  
+# end
+  
+#   lil_bub = Animal.new("cat") 
+# Answer: lil_bub is an instance of Animal
+  
+
